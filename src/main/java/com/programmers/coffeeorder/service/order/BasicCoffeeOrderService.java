da0896c4861ed9bf467243e7cf63e9c6d030a063
Kwonkyu <park2348190@naver.com>
2021-09-17 03:49:51 +0900

    Updated coffee order service.
    - CoffeeOrderRepository를 활용하는 서비스 구현

--- src/main/java/com/programmers/coffeeorder/service/order/BasicCoffeeOrderService.java
+++ src/main/java/com/programmers/coffeeorder/service/order/BasicCoffeeOrderService.java
@@ -1,9 +1,7 @@
-package com.programmers.coffeeorder.service;
+package com.programmers.coffeeorder.service.order;
 
-import com.programmers.coffeeorder.controller.bind.CoffeeOrderSubmit;
 import com.programmers.coffeeorder.entity.CoffeeOrder;
-import com.programmers.coffeeorder.entity.Order;
-import com.programmers.coffeeorder.repository.CoffeeOrderRepository;
+import com.programmers.coffeeorder.repository.order.CoffeeOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,26 +13,24 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class CoffeeOrderService implements OrderService {
+public class BasicCoffeeOrderService implements CoffeeOrderService {
 
     private final CoffeeOrderRepository coffeeOrderRepository;
 
     @Override
-    public Optional<Order.DTO> submitOrder(Object submit) {
-        if(!(submit instanceof CoffeeOrder)) return Optional.empty();
-        CoffeeOrder request = (CoffeeOrder) submit;
-        CoffeeOrder order = coffeeOrderRepository.createOrder(request);
+    public Optional<CoffeeOrder.DTO> submitOrder(CoffeeOrder submit) {
+        CoffeeOrder order = coffeeOrderRepository.createOrder(submit);
         return Optional.ofNullable(order.toDTO());
     }
 
     @Override
-    public List<? extends Order.DTO> listOrdersBetweenTime(LocalDateTime from, LocalDateTime to) {
+    public List<CoffeeOrder.DTO> listOrdersBetweenTime(LocalDateTime from, LocalDateTime to) {
         List<CoffeeOrder> coffeeOrders = coffeeOrderRepository.listOrdersBetween(from, to);
         return coffeeOrders.stream().map(CoffeeOrder::toDTO).collect(Collectors.toList());
     }
 
     @Override
-    public List<? extends Order.DTO> getDeliveryReservedOrders(LocalDate date) {
+    public List<CoffeeOrder.DTO> getDeliveryReservedOrders(LocalDate date) {
         List<CoffeeOrder> coffeeOrders = coffeeOrderRepository.listDeliveryReservedOrders(date);
         return coffeeOrders.stream().map(CoffeeOrder::toDTO).collect(Collectors.toList());
     }
---
+++ src/main/java/com/programmers/coffeeorder/service/order/CoffeeOrderService.java
@@ -0,0 +1,17 @@
+package com.programmers.coffeeorder.service.order;
+
+import com.programmers.coffeeorder.entity.CoffeeOrder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface CoffeeOrderService {
+
+    Optional<CoffeeOrder.DTO> submitOrder(CoffeeOrder submit);
+
+    List<CoffeeOrder.DTO> listOrdersBetweenTime(LocalDateTime from, LocalDateTime to);
+
+    List<CoffeeOrder.DTO> getDeliveryReservedOrders(LocalDate date);
+}
