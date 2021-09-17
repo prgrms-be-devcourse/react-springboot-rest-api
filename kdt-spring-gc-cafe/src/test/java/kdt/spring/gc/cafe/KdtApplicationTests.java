b481ed82514c4d9927b4cf3ed3eac36ea631474d
yhh1056 <yhh1056@naver.com>
2021-09-17 16:06:32 +0900

    Build: spring project init

---
+++ kdt-spring-gc-cafe/src/test/java/kdt/spring/gc/cafe/KdtApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.spring.gc.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
