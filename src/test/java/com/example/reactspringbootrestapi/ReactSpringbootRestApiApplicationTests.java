d635cb666fefced8718b6f25fe5c0d143881e655
wisehero <godjw96@gmail.com>
2022-04-24 18:35:11 +0900

    # Project Settings

---
+++ src/test/java/com/example/reactspringbootrestapi/ReactSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
