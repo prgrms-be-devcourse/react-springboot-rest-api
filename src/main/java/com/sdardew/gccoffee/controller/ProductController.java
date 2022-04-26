5937866687743506f6ce121a87b98493d36bb8c3
sdardew <sdardew@gmail.com>
2022-04-26 17:00:02 +0900

    에러 코드

--- src/main/java/com/sdardew/gccoffee/controller/ProductController.java
+++ src/main/java/com/sdardew/gccoffee/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.sdardew.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -19,5 +22,20 @@ public class ProductController {
     var products = productService.getAllProducts();
     model.addAttribute("products", products);
     return "product-list";
+  }
+
+  @GetMapping("/new-product")
+  public String newProductPage() {
+    return "new-product";
+  }
+
+  @PostMapping("/products")
+  public String newProduct(CreateProductRequest createProductRequest) {
+    productService.createProduct(
+      createProductRequest.productName(),
+      createProductRequest.category(),
+      createProductRequest.price(),
+      createProductRequest.description());
+    return "redirect:/products";
   }
 }
