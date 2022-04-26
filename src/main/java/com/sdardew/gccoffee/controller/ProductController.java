1bafd15259f8c0625ed68b5d9c8b5c28a399287f
sdardew <sdardew@gmail.com>
2022-04-26 17:34:23 +0900

    error: 문제 해결

--- src/main/java/com/sdardew/gccoffee/controller/ProductController.java
+++ src/main/java/com/sdardew/gccoffee/controller/ProductController.java
@@ -4,9 +4,7 @@ import com.sdardew.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
