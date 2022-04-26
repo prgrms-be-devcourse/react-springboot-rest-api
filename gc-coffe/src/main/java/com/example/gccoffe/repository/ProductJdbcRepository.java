11c779b2d31cf510f7dd84d2efab4f667f8f29b9
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-27 03:58:43 +0900

    Feat: ProductJdbcRepository&test 생성 - find 부분

---
+++ gc-coffe/src/main/java/com/example/gccoffe/repository/ProductJdbcRepository.java
@@ -0,0 +1,122 @@
+package com.example.gccoffe.repository;
+
+import com.example.gccoffe.model.Category;
+import com.example.gccoffe.model.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.yaml.snakeyaml.tokens.ScalarToken;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static com.example.gccoffe.util.JdbcUtils.toLocalDateTime;
+import static com.example.gccoffe.util.JdbcUtils.toUUID;
+
+@Repository
+public class ProductJdbcRepository implements ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
+        UUID productId = toUUID(resultSet.getBytes("product_id"));
+        String productName = resultSet.getString("product_name");
+        Category category = Category.valueOf(resultSet.getString("category"));
+        Long price = resultSet.getLong("price");
+        String description = resultSet.getString("description");
+        LocalDateTime createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+        LocalDateTime updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
+
+
+    private Map<String,Object> toParamMap(Product product) {
+        // 이전 강의에서 이중 괄호 초기화 (double braces initializer)를 사용했음
+        // 장점: syntax 편리함
+        // 단점: inner class가 만들어져서 메모리 누수(memory leak)이 발생할 수 있음
+        // new HashMap<>() {{
+        //     ...
+        // }};
+
+        // java 9에서 추가된 팩토리 방법(List.of(), Map.of() 등)을 사용할 수도 있지만 null 요소로 목록을 만드려고 하면 예외 발생함
+
+        // 그냥 이렇게 하는게 깔끔
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("productId", product.getProductId().toString().getBytes());
+        paramMap.put("productName", product.getProductName());
+        paramMap.put("category", product.getCategory().toString());
+        paramMap.put("price", product.getPrice());
+        paramMap.put("description", product.getDescription());
+        paramMap.put("createdAt", product.getCreatedAt());
+        paramMap.put("updatedAt", product.getUpdatedAt());
+        return paramMap;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("select * from products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        int update = jdbcTemplate.update("INSERT INTO products(product_id,product_name, category, price, description, created_at, updated_at)" +
+                " VALUES(UUID_TO_BIN(:productId), :productName, :category, :price, :description, :createdAt, :updatedAt)", toParamMap(product));
+
+        // insert된게 하나가 아니면 예외 발생시킴
+        if (update != 1) {
+            throw new RuntimeException("Nothing was inserted");
+        }
+
+        return product;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        // 결과가 없는 경우 예외 발생
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UUID_TO_BIN(:productId)",
+                            Collections.singletonMap("productId", productId.toString().getBytes()), productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        // 결과가 없는 경우 예외 발생
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                            Collections.singletonMap("productName", productName), productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return jdbcTemplate.query(
+                "SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category", category.toString()),
+                productRowMapper
+        );
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
---
+++ gc-coffe/src/test/java/com/example/gccoffe/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.example.gccoffe.repository;
+
+import com.example.gccoffe.model.Category;
+import com.example.gccoffe.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.wix.mysql.distribution.Version.v8_0_11;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.MysqldConfig.*;
+
+// 알아서 SpringBootApplication을 찾아줌 -> 루트의 @SpringBootConfiguration을 찾아
+// bean 설정을 읽어서 container를 만들어줌
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)     // 예) 클래스마다 newProduct 인스턴스가 생김
+class ProductJdbcRepositoryTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        MysqldConfig config = aMysqldConfig(v8_0_11)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+
+    @Autowired
+    ProductRepository repository;
+
+    // static으로 해야지 테스트마다 새 인스턴스가 생성되지 않음
+    private static final Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+    @Test
+    @Order(1)
+    @DisplayName("상품을 추가할 수 있다.")
+    void testInsert() {
+        repository.insert(newProduct);
+        List<Product> all = repository.findAll();
+        assertThat(all.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("상품을 이름으로 조회할 수 있다.")
+    void testFindByName() {
+        Optional<Product> product = repository.findByName(newProduct.getProductName());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품을 아이디로 조회할 수 있다.")
+    void testFindById() {
+        Optional<Product> product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("상품들을 카테고리으로 조회할 수 있다.")
+    void testFindByCategory() {
+        List<Product> product = repository.findByCategory(Category.COFFEE_BEAN_PACKAGE);
+        assertThat(product.isEmpty(), is(false));
+    }
+}
