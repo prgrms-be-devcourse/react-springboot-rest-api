1b22c594fb694ab3c84c77545290c8d8f51f6568
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-27 00:37:49 +0900

    Init project

---
+++ gc-coffe/src/main/java/com/example/gccoffe/GcCoffeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gccoffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcCoffeApplication.class, args);
+    }
+
+}
---
+++ gc-coffe/src/test/java/com/example/gccoffe/GcCoffeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gccoffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
