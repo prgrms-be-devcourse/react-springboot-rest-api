bc489ec45551eb4f420ac167cca1c11eccd746f8
San Kim <waterfog9580@gmail.com>
2022-04-26 12:54:56 +0900

    :sparkles: Add: 도메인 작성

---
+++ src/test/java/com/example/gccoffee/model/EmailTest.java
@@ -0,0 +1,21 @@
+package com.example.gccoffee.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+    @Test
+    public void 이메일_테스트_실패() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Email("accc");
+        });
+    }
+
+    @Test
+    public void 이메일_테스트_성공() {
+        final var email = new Email("hello@gamil.com");
+        assertEquals("hello@gamil.com", email.getAddress());
+    }
+
+}
