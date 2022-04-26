e56f5ceaccab04652b2ba7f6b08ea17bc1688a9a
hyuk0309 <bahngeh@naver.com>
2022-04-26 15:50:08 +0900

    W7D2 클론코딩 - React

---
+++ kdt-react-order-ui/src/App.js
@@ -0,0 +1,47 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import React, {useEffect, useState} from 'react'
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
+
+function App() {
+  const [products, setProducts] = useState([
+    {id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 5000},
+    {id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 5000},
+    {id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 5000},
+  ]);
+  const [items, setItems] = useState([]);
+  const handleAddClicked = id => {
+    const product = products.find(v => v.id === id);
+    const found = items.find(v => v.id === id);
+    const update =
+        found ? items.map(v => (v.id === id) ?  {...v, count: v.count + 1 } : v) : [...items, { ...product, count: 1}]
+    setItems(update);
+    console.log(products.find(v => v.id === id), "added!");
+  }
+  useEffect(() =>{
+    axios.get('http://localhost:8080/api/v1/products')
+      .then(v => setProducts(v.data));
+  }, []);
+  return (
+      <div className="container-fluid">
+        <div className="row justify-content-center m-4">
+          <h1 className="text-center">Grids & Circle</h1>
+        </div>
+        <div className="card">
+          <div className="row">
+            <div
+                className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+              <ProductList products={products} onAddClick={handleAddClicked}/>
+            </div>
+            <div className="col-md-4 summary p-4">
+              <Summary items={items}/>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ kdt-react-order-ui/src/components/Summary.js
@@ -0,0 +1,42 @@
+import {SummaryItem} from "./SummaryItem";
+import React from "react";
+
+export function Summary({items = []}) {
+  const totalPrice = items.reduce((prev, curr) =>
+      prev + (curr.price * curr.count), 0);
+  return (
+      <>
+        <div>
+          <h5 className="m-0 p-0"><b>Summary</b></h5>
+        </div>
+        <hr/>
+        {items.map(v =>
+            <SummaryItem key={v.id} count={v.count}
+                         productName={v.productName}/>
+        )}
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">이메일</label>
+            <input type="email" className="form-control mb-1"
+                   id="email"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">주소</label>
+            <input type="text" className="form-control mb-1"
+                   id="address"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="postcode"
+                   className="form-label">우편번호</label>
+            <input type="text" className="form-control" id="postcode"/>
+          </div>
+          <div>당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.</div>
+        </form>
+        <div className="row pt-2 pb-2 border-top">
+          <h5 className="col">총금액</h5>
+          <h5 className="col text-end">{totalPrice}원</h5>
+        </div>
+        <button className="btn btn-dark col-12">결제하기</button>
+      </>
+  )
+}
