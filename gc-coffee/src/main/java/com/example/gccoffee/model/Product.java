31ff7be073fc210301c2a7bebb961287eb60ff47
jki503 <jki503@naver.com>
2022-04-26 09:42:46 +0900

    feat : product model 추가
    product 규칙에 맞춰서 product 생성
    product의 type을 Category enum으로 정의

---
+++ gc-coffee/src/main/java/com/example/gccoffee/model/Product.java
@@ -0,0 +1,57 @@
+package com.example.gccoffee.model;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class Product {
+    private final UUID productId;
+    private String productName;
+    private Category category;
+    private long price; // 원화만 지원하기때문에 소수점 신경 X
+    private String description;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(UUID productId, String productName, Category category, long price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Product(UUID productId, String productName, Category category, long price, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
