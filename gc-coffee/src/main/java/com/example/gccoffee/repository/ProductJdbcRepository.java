2392e7e8c2a15406d05b71b8cb8050e49c57bc64
jki503 <jki503@naver.com>
2022-04-26 16:38:25 +0900

    feat : ProductJdbcRepository.java
    - insert
    - findById
    - findByName
    - findByCategory
    - update
    - deleteAll
    
    기능 추가
    
    test : ProductJdbcRepositoryTest.java
    
    embedded Mysql로 ProductJdbcRepository 테스트

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -2,16 +2,17 @@ package com.example.gccoffee.repository;
 
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static com.example.gccoffee.JdbcUtils.*;
 
+@Repository
 public class ProductJdbcRepository implements ProductRepository {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
@@ -27,35 +28,67 @@ public class ProductJdbcRepository implements ProductRepository {
 
     @Override
     public Product insert(Product product) {
-        return null;
+        int update = jdbcTemplate.update("INSERT INTO products(product_id, product_name, category, price, description, created_at, updated_at)"
+                        + " VALUES(UUID_TO_BIN(:productId), :productName, :category, :price, :description, :createdAt, :updatedAt)",
+                toParamMap(product));
+
+        if (update != 1)
+            throw new RuntimeException("Nothing was inserted");
+
+        return product;
     }
 
     @Override
     public Product update(Product product) {
-        return null;
+        int update = jdbcTemplate.update("UPDATE products SET product_name = :productName, category = :category, price = :price, description = :description, created_at = :createdAt, updated_at = :updatedAt"
+                        +  " WHERE product_id = UUID_TO_BIN(:productId)",
+                toParamMap(product));
+
+        if(update != 1)
+            throw new RuntimeException("Nothing was updated");
+
+        return product;
     }
 
     @Override
     public Optional<Product> findById(UUID productId) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UUID_TO_BIN(:productId)",
+                            Collections.singletonMap("productId", productId.toString().getBytes()),
+                            productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
-    public Optional<Product> findByName(String name) {
-        return Optional.empty();
+    public Optional<Product> findByName(String productName) {
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                            Collections.singletonMap("productName", productName),
+                            productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Product> findByCategory(Category category) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category", category.toString()),
+                productRowMapper);
     }
 
     @Override
     public void deleteAll() {
-
+        jdbcTemplate.update("DELETE FROM products", Collections.emptyMap());
     }
 
-    private static final RowMapper<Product >productRowMapper = (resultSet, i) ->{
+    private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
         UUID productId = toUUID(resultSet.getBytes("product_id"));
         String ProductName = resultSet.getString("product_name");
         Category category = Category.valueOf(resultSet.getString("category"));
@@ -67,6 +100,16 @@ public class ProductJdbcRepository implements ProductRepository {
         return new Product(productId, ProductName, category, price, description, createdAt, updatedAt);
     };
 
+    private Map<String, Object> toParamMap(Product product) {
+        var paramMap = new HashMap<String, Object>();
+        paramMap.put("productId", product.getProductId().toString().getBytes());
+        paramMap.put("productName", product.getProductName());
+        paramMap.put("category", product.getCategory().toString());
+        paramMap.put("price", product.getPrice());
+        paramMap.put("description", product.getDescription());
+        paramMap.put("createdAt", product.getCreatedAt());
+        paramMap.put("updatedAt", product.getUpdatedAt());
 
-
+        return paramMap;
+    }
 }
