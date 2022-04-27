2b31f18aea03b453d6504abfbcc764bf7173267c
wisehero <godjw96@gmail.com>
2022-04-27 14:20:45 +0900

    # 컨트롤러 작성

---
+++ src/main/java/com/example/reactspringbootrestapi/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.reactspringbootrestapi.controller;
+
+import com.example.reactspringbootrestapi.model.Product;
+import com.example.reactspringbootrestapi.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        var products = productService.getAllProduct();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+    @GetMapping("new-product")
+    public String newProductPage() {
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(
+                createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
+    }
+}
