6943f8ddd69e31bae6f1bef16e5f9fac002afb97
wisehero <godjw96@gmail.com>
2022-04-27 17:11:19 +0900

    # record 작성

--- src/main/java/com/example/reactspringbootrestapi/model/OrderItem.java
+++ src/main/java/com/example/reactspringbootrestapi/model/OrderItem.java
@@ -1,4 +1,6 @@
 package com.example.reactspringbootrestapi.model;
 
-public class OrderItem {
+import java.util.UUID;
+
+public record OrderItem(UUID productId, Category category, long price, int quantity) {
 }
