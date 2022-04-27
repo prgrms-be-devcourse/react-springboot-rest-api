5ba45bdf528f556206401ad7b47186e21e2e104b
HyeonseoJUNG <windymay1920@gmail.com>
2022-04-27 16:20:47 +0900

    Feat: ProductController productsPage 기능 구현

---
+++ gc-coffe/src/main/java/com/example/gccoffe/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.example.gccoffe.controller;
+
+import com.example.gccoffe.model.Product;
+import com.example.gccoffe.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+}
