25d61742d8d360d8d2cffda883f7992599d36831
San Kim <waterfog9580@gmail.com>
2022-04-27 16:14:04 +0900

    :sparkles: Feat: Controller 및 뷰

---
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.gccoffee.contoroller;
+
+import com.example.gccoffee.controller.CreateProductRequest;
+import com.example.gccoffee.model.Product;
+import com.example.gccoffee.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+    @GetMapping("new-product")
+    public String newProduct() {
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
+    }
+}
