8fdf53f6c44facfb3671134e620a98aa4ffd477e
Juhan <juhan211@naver.com>
2022-04-27 10:04:12 +0900

    W7D2 - 상품관리 REST API 클론하기(with React) -까지의 설정정보

---
+++ gc-coffee/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+-- DROP TABLE products;
+/*
+CREATE TABLE products
+(
+product_id   BINARY(16)  PRIMARY KEY,
+product_name VARCHAR(20) NOT NULL,
+category     VARCHAR(50) NOT NULL,
+price        BIGINT      NOT NULL,
+description  VARCHAR(500) DEFAULT NULL,
+create_at    DATETIME(6) NOT NULL,
+update_at    DATETIME(6) DEFAULT NULL
+);
+*/
