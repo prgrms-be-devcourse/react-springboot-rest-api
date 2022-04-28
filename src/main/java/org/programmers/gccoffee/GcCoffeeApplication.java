095935174b8b21c8f9620c0714911e203111ac06
Hanbin Lee <logos1018@gmail.com>
2022-04-28 19:09:38 +0900

    init: 스프링부트 프로젝트 생성

---
+++ src/main/java/org/programmers/gccoffee/GcCoffeeApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.gccoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programmers/gccoffee/GcCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.gccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
