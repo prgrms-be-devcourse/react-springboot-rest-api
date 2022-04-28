06b633d581173445cfbf423ab4c82c98c109f27c
sdardew <sdardew@gmail.com>
2022-04-28 20:54:19 +0900

    주문 서버에 전송

--- gc-coffee-order-ui/src/App.js
+++ gc-coffee-order-ui/src/App.js
@@ -7,25 +7,49 @@ import axios from "axios";
 
 function App() {
     const [products, setProducts] = useState([
-        { id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 3000 },
-        { id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 5000 },
-        { id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 7000 },
+        {productId: 'uuid-1', productName: '콜롬비아 커피 1', category: '커피빈', price: 5000},
+        {productId: 'uuid-2', productName: '콜롬비아 커피 2', category: '커피빈', price: 5000},
+        {productId: 'uuid-3', productName: '콜롬비아 커피 3', category: '커피빈', price: 5000},
     ]);
-
     const [items, setItems] = useState([]);
-    const handleAddClicked = id => {
-        const product = products.find(v => v.id === id);
-        const found = items.find(v => v.id === id);
+    const handleAddClicked = productId => {
+        const product = products.find(v => v.productId === productId);
+        const found = items.find(v => v.productId === productId);
         const updatedItems =
-            found ? items.map(v => (v.id === id) ? { ...v, count: v.count + 1} : v) : [...items, { ...product, count: 1 }]
+            found ? items.map(v => (v.productId === productId) ? {...v, count: v.count + 1} : v) : [...items, {
+                ...product,
+                count: 1
+            }]
         setItems(updatedItems);
-        console.log(products.find(v => v.id === id), "added!");
     }
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/v1/products')
             .then(v => setProducts(v.data));
     }, []);
+
+    const handleOrderSubmit = (order) => {
+        if (items.length === 0) {
+            alert("아이템을 추가해 주세요!");
+        } else {
+            axios.post('http://localhost:8080/api/v1/orders', {
+                email: order.email,
+                address: order.address,
+                postcode: order.postcode,
+                orderItems: items.map(v => ({
+                    productId: v.productId,
+                    category: v.category,
+                    price: v.price,
+                    quantity: v.count
+                }))
+            }).then(
+                v => alert("주문이 정상적으로 접수되었습니다."),
+                e => {
+                    alert("서버 장애");
+                    console.error(e);
+                })
+        }
+    }
 
     return (
         <div className="container-fluid">
@@ -35,10 +59,10 @@ function App() {
             <div className="card">
                 <div className="row">
                     <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-                        <ProductList products={products} onAddClick={handleAddClicked}></ProductList>
+                        <ProductList products={products} onAddClick={handleAddClicked}/>
                     </div>
                     <div className="col-md-4 summary p-4">
-                        <Summary items={items} />
+                        <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
                     </div>
                 </div>
             </div>
