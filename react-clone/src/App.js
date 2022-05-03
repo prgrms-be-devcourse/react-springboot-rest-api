a4da3e0857bae8fa16276251e69415c8be126599
yeon <soccer_by@naver.com>
2022-05-03 16:42:07 +0900

    feat: 주문 ui 작성 및 end point 연결

--- react-clone/src/App.js
+++ react-clone/src/App.js
@@ -8,31 +8,54 @@ import axios from "axios";
 function App() {
     // {'상태에 접근할 수 있는 변수명','상태값 바구는 함수'}
     const [products, setProducts] = useState([
-        {id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: '3000'},
-        {id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: '5000'},
-        {id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: '5500'}
+        {productId: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: '3000'},
+        {productId: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: '5000'},
+        {productId: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: '5500'}
     ]);
 
     const [items, setItems] = useState([]);
 
-    const handleAddClicked = id => {
-        console.log(products.find(v => v.id === id));
-        const product = products.find(v => v.id === id);
-        const found = items.find(v => v.id === id);
+    const handleAddClicked = productId => {
+        console.log(products.find(v => v.productId === productId));
+        const product = products.find(v => v.productId === productId);
+        const found = items.find(v => v.productId === productId);
 
-        const updatedItems = found ? items.map(v => (v.id === id) ? {
+        const updatedItems = found ? items.map(v => (v.productId === productId) ? {
             ...v,
             count: v.count + 1
         } : v) : [...items, {...product, count: 1}]
         setItems(updatedItems);
-        console.log(id, "clicked !")
+        console.log(productId, "clicked !")
     }// 랜더링과 리액트 컴포넌트 호출에서 갭이 생김
     useEffect(() => {
         // 비동기 작업은 모두 여기서
         axios.get("http://localhost:8080/api/v1/products")
-            .then(v=>setProducts(v.data));
-    },[]);
+            .then(v => setProducts(v.data));
+    }, []);
 
+    const handleOrderSubmit = (order) => {
+        console.log("submit!")
+        if (items.length === 0) {
+            alert("아이템을 추가해 주세요");
+            return;
+        }
+        axios.post("http://localhost:8080/api/v1/orders", {
+            email: order.email,
+            address: order.address,
+            postcode: order.postcode,
+            orderItems:
+                items.map(v => ({
+                    productId: v.productId,
+                    category: v.category,
+                    price: v.price,
+                    quantity: v.count
+                }))
+        }).then(v => alert("주문 처리 완료!"),e => {
+            alert("서버에 문제가 생겼습니다.");
+            console.log(e);
+        })
+        console.log(order, items);
+    };
     return (
         <div className="container-fluid">
             <div className="row justify-content-center m-4">
@@ -44,7 +67,7 @@ function App() {
                         <ProductList products={products} onAddClick={handleAddClicked}/>
                     </div>
                     <div className="col-md-4 summary p-4">
-                        <Summary items={items}/>
+                        <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
                     </div>
                 </div>
             </div>
--- react-clone/src/components/Summary.js
+++ react-clone/src/components/Summary.js
@@ -1,28 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import {SummaryItem} from "./SummaryItem";
 
 
-export function Summary({items = []}) {
+export function Summary({items = [], onOrderSubmit}) {
     const totalPrice = items.reduce((prev, cur) => prev + (cur.price * cur.count), 0);
+    const [order, setOrder] = useState({
+        email: "", address: "", postcode: ""
+    });
+    const handleChangeEmailInputChanged = (e) => {
+        const value = e.target.value;
+        console.log(value);
+        setOrder({
+            ...order, email: e.target.value
+        })
+    }
+
+    const handleAddressInputChanged = (e) => {
+        const value = e.target.value;
+        console.log(value);
+        setOrder({
+            ...order, address: e.target.value
+        })
+    }
+
+    const handlePostcodeInputChanged = (e) => {
+        const value = e.target.value;
+        console.log(value);
+        setOrder({
+            ...order, postcode: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        if(order.address==="" || order.email==="" || order.postcode ===""){
+            alert("입력 값을 확인해주세요.");
+            return;
+        }
+
+        // order 정보랑 합쳐서 request 해야함 : props
+        console.log(order);
+
+        onOrderSubmit(order);
+
+    }
+
     return (
         <>
             <div>
                 <h5 className="m-0 p-0"><b>Summary</b></h5>
             </div>
             <hr/>
-            {items.map(v => <SummaryItem key={v.id} count={v.count} productName={v.productName}/>)}
+            {items.map(v => <SummaryItem key={v.productId} count={v.count} productName={v.productName}/>)}
             <form>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">이메일</label>
-                    <input type="email" className="form-control mb-1" id="email"/>
+                    <input type="email" className="form-control mb-1" value={order.email}
+                           onChange={handleChangeEmailInputChanged} id="email"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="address" className="form-label">주소</label>
-                    <input type="text" className="form-control mb-1" id="address"/>
+                    <input type="text" className="form-control mb-1" value={order.address}
+                           onChange={handleAddressInputChanged} id="address"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="postcode" className="form-label">우편번호</label>
-                    <input type="text" className="form-control" id="postcode"/>
+                    <input type="text" className="form-control" value={order.postcode}
+                           onChange={handlePostcodeInputChanged} id="postcode"/>
                 </div>
                 <div>당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.</div>
             </form>
@@ -30,7 +73,7 @@ export function Summary({items = []}) {
                 <h5 className="col">총금액</h5>
                 <h5 className="col text-end">{totalPrice}원</h5>
             </div>
-            <button className="btn btn-dark col-12">결제하기</button>
+            <button className="btn btn-dark col-12" onClick={handleSubmit}>결제하기</button>
         </>
     )
 }
