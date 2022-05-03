91611645ffe290b05c35816ca2a3e33b4f9769f3
dojinyou <djyou128@gmail.com>
2022-05-03 11:00:04 +0900

    :sparkles: feat: Order model 구현

---
+++ gc-coffee/src/main/java/com/dojinyou/devcourse/gccoffee/model/Email.java
@@ -0,0 +1,45 @@
+package com.dojinyou.devcourse.gccoffee.model;
+
+import org.springframework.util.Assert;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Email {
+    private final String address;
+
+
+    public Email(String address) {
+        Assert.notNull(address, "address should not be null");
+        Assert.isTrue(address.length() >= 4 && address.length() <= 50, "address length must be between 4 and 50 characters");
+        Assert.isTrue(checkAdress(address),"Invalid email address");
+        this.address = address;
+    }
+
+    private static boolean checkAdress(String address) {
+        return Pattern.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b",address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(address, email.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" + "address='" + address + '\'' +'}';
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ gc-coffee/src/main/java/com/dojinyou/devcourse/gccoffee/model/OrderItem.java
@@ -0,0 +1,4 @@
+package com.dojinyou.devcourse.gccoffee.model;
+
+public class OrderItem {
+}
