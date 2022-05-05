06f4ba09254fd61138ffbf72e17307b1a3eda350
yongckim <zxcv9203@naver.com>
2022-05-06 00:16:46 +0900

    feat : product service update impl

--- src/main/java/com/devcourse/drink/product/service/ProductService.java
+++ src/main/java/com/devcourse/drink/product/service/ProductService.java
@@ -4,6 +4,7 @@ import com.devcourse.drink.product.model.Category;
 import com.devcourse.drink.product.model.Product;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductService {
@@ -12,6 +13,8 @@ public interface ProductService {
     List<Product> getAllProduct();
 
     Product createProduct(String name, Category category, long price, String description);
+
+    Product updateProduct(UUID productId, String name, Category category, long price, String description);
 
     void deleteProduct(UUID productId);
 }
--- src/main/java/com/devcourse/drink/product/service/ProductServiceImpl.java
+++ src/main/java/com/devcourse/drink/product/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.devcourse.drink.product.service;
 
+import com.devcourse.drink.config.error.ErrorType;
+import com.devcourse.drink.config.exception.OrderNotMatchedException;
 import com.devcourse.drink.product.model.Category;
 import com.devcourse.drink.product.model.Product;
 import com.devcourse.drink.product.repository.ProductRepository;
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
+import static com.devcourse.drink.config.error.ErrorType.ORDER_NOT_MATCHED;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -35,6 +40,18 @@ public class ProductServiceImpl implements ProductService {
 
         return productRepository.insert(product);
     }
+
+    @Override
+    public Product updateProduct(UUID productId, String name, Category category, long price, String description) {
+        Optional<Product> product = productRepository.findById(productId);
+
+        if (product.isPresent()) {
+            return productRepository.update(product.get());
+        } else {
+            throw new OrderNotMatchedException(ORDER_NOT_MATCHED);
+        }
+    }
+
 
     @Override
     public void deleteProduct(UUID productId) {
