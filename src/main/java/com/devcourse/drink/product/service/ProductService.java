745663ea08b6af9208f711ba2a9e78590e25e56c
yongckim <zxcv9203@naver.com>
2022-05-05 23:37:40 +0900

    feat : product service impl

---
+++ src/main/java/com/devcourse/drink/product/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.devcourse.drink.product.service;
+
+import com.devcourse.drink.product.model.Category;
+import com.devcourse.drink.product.model.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    List<Product> getProductByCategory(Category category);
+
+    List<Product> getAllProduct();
+
+    Product createProduct(String name, Category category, long price, String description);
+
+    void deleteProduct(UUID productId);
+}
