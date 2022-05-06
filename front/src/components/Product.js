7b8d3b1fa49ea61adef2ec1c1d2101875e28f5d9
yongckim <zxcv9203@naver.com>
2022-05-06 19:55:12 +0900

    feat : frontend(react) impl

---
+++ front/src/components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function Product(props) {
+    const id = props.productId;
+    const name = props.name;
+    const category = props.category;
+    const price = props.price;
+    const handleAddBtnClicked = e => {
+        props.onAddClick(id);
+    };
+    return (
+        <>
+            <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/>
+            </div>
+            <div className="col">
+                <div className="row text-muted">{category}</div>
+                <div className="row">{name}</div>
+            </div>
+            <div className="col text-center price">{price}원</div>
+            <div className="col text-end action">
+                <button onClick={(handleAddBtnClicked)} className="btn btn-small btn-outline-dark">추가</button>
+            </div>
+        </>
+    )
+}
