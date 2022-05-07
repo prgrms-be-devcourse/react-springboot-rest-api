ef7ae632edb24998362aa30b5ae98da6f6d9626f
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-05-07 18:40:00 +0900

    feat: Email, ProductRepository 테스트 코드 작성

---
+++ gc-coffee/src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,112 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.MysqldConfig.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ProductJdbcRepositoryTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        var config = aMysqldConfig(Version.v8_0_11)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+
+    }
+
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+
+    @Autowired
+    ProductRepository repository;
+
+    // static 으로 해야 된다. 아니면 test 메소드마다 인스턴스가 생성이 된다!
+    // 혹은 TestInstance 어노태이션을 활용하자 - 메소드에서 클래스로 단위를 바꾸자
+    private Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+    @Test
+    @Order(1)
+    @DisplayName("상품을 추가할 수 있다.")
+    void testInsert() {
+        repository.insert(newProduct);
+        var all = repository.finaAll();
+        assertThat(all.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("상품을 이름으로 조회할 수 있다.")
+    void testFindByName() {
+        var product = repository.findByName(newProduct.getProductName());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품을 아이디로 조회할 수 있다.")
+    void testFindById() {
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("상품들을 카데코리로 조회할 수 있다.")
+    void testFindByCategory() {
+        var product = repository.findByCategory(Category.COFFEE_BEAN_PACKAGE);
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("상품을 수정할 수 있다.")
+    void testUpdate(){
+        newProduct.setProductName("updated-product");
+        repository.update(newProduct);
+
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+        assertThat(product.get(), samePropertyValuesAs(newProduct));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("상품을 전체 삭제한다.")
+    void testDeleteAll() {
+        repository.deleteAll();
+        var all = repository.finaAll();
+        assertThat(all.isEmpty(), is(true));
+    }
+}
