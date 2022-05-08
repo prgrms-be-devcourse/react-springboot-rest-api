be29ff33ec15bddb06198a33fe300b4ebeeced69
chlwlgns524 <chlwlgns524@naver.com>
2022-05-08 12:46:28 +0900

    CHORE: 스프링 부트 프로젝트 초기 파일 생성

---
+++ src/main/java/com/example/myfirstfullstackproject/MyFirstFullstackProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.myfirstfullstackproject;
+
+import com.example.myfirstfullstackproject.model.Category;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstFullstackProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstFullstackProjectApplication.class, args);
+    }
+
+}
